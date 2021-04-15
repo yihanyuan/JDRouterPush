3dee3f21472fab7d5be715a118960d83dca29789
yihanyuan <33946365+yihanyuan@users.noreply.github.com>
2021-04-15 18:30:41 +0800

    Update GlobalVariable.py
    二代ax1800 64g路由器获取插件参数修改

--- GlobalVariable.py
+++ GlobalVariable.py
@@ -28,7 +28,7 @@ cmds = [
     "get_device_list",    # 获取设备列表 在线与离线的客户端状态
     "get_router_status_info",    # 获取路由器状态信息 上传与下载
     "get_router_status_detail",    # 获取路由器版本 mac  sn  上传  下载  cpu  路由在线时间(秒)  wanip  内存
-    "jdcplugin_opt.get_pcdn_status",    # 获取路由器插件版本   缓存大小
+    "plugin.jdcplugin_opt.get_pcdn_status",    # 获取路由器插件版本   缓存大小
 ]
 
 # 请求参数
